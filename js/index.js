22f8266dc7b698105888841a7ae534ed17627334
Garance Defrel <garance972@hotmail.com>
2023-04-13 16:47:29 +0200

    second commit

--- js/index.js
+++ js/index.js
@@ -1,21 +1,26 @@
-const list = document.querySelector('ul');
+// On utilise document.querySelector() pour sélectionner l'élément HTML par son ID
+const list = document.querySelector("ul");
 
-fetch('./data.json').then(response => {
-  return response.json();
-}).then(data => {
-  // Work with JSON data here
-  for (let i = 0; i < data.length; i++) {
-    const category = data[i].category;
-    const score = data[i].score;
-    const icon = data[i].icon;
+// On utilise fetch() pour récupérer le document JSON et travailler avec les données
+fetch("./data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    // On utilise forEach() pour parcourir chaque objet dans le document JSON
+    data.forEach((item) => {
+      const category = item.category;
+      const score = item.score;
+      const icon = item.icon;
 
-    const listItem = document.createElement('li');
-    listItem.innerHTML = `
-    <img src="${icon}" alt="${category}" />
-    <h2>${category}</h2>
-    <p>${score}</p>`;
-    list.appendChild(listItem);
+      // On crée un élément HTML pour afficher les données
+      const listItem = document.createElement("li");
+      listItem.classList.add("summary-item", `summary-item--${category.toLowerCase()}`);
+      listItem.innerHTML = `
+        <img src="${icon}" alt="${category} icon">
+        <h2 class="summary-category">${category}</h2>
+        <p class="summary-score">${score}<span>/100</span></p>
+      `;
 
-    
-  }
-  })
+      // On ajoute l'élément HTML à la liste
+      list.appendChild(listItem);
+    });
+  });
